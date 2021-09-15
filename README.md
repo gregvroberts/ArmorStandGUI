43bf53512a8c7e8f42d5e092cb098be06dd96760
Gregory Roberts <roberts.gregory@heb.com>
2021-09-15 15:29:47 -0500

    Init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#Armor Stand GUI
+Spigot Version: 1.17.1
+Created by: gregvroberts
+
---
+++ src/main/java/mc/gregvroberts/armorstandgui/ArmorStandGUI.java
@@ -0,0 +1,17 @@
+package mc.gregvroberts.armorstandgui;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ArmorStandGUI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
